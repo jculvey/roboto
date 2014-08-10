1a4912727cc1730dade66b8e5b9dc41b42510382
James Culveyhouse <jculveyhouse@riverbed.com>
2014-08-09 18:52:37 -0700

    Removing trash

--- example/pipelines/magpie-item-logger.js
+++
@@ -1,5 +0,0 @@
-exports = function(options){
-  return function(item) {
-    console.log(JSON.stringify(item));
-  };
-};
--- example/pipelines/magpie-solr.js
+++
@@ -1,6 +0,0 @@
-exports = function(options){
-  return function(item) {
-    // write item to solr
-    console.log("Writing item to solr");
-  };
-};
