fe5f5ca586c8301f08df1ecb624431dc9a0d744a
James Culveyhouse <jculveyhouse@gmail.com>
2014-08-14 00:42:47 -0700

    Improvements to url normalization

--- test/urls.js
+++ test/urls.js
@@ -36,13 +36,73 @@ describe('Url Normalization', function(){
     assert.equal(actual, expected);
   });
 
-  //it('Resolve remove extra //', function(){
-    //var resp = {};
-    //var link = $("<a href='/stories//foo.html'>");
-    //resp.url = "http://www.foonews.com";
-    //var actual = testCrawler._normalizeUrl(link, resp);
-    //var expected = "http://www.foonews.com/stories/foo.html";
-    //assert.equal(actual, expected);
-  //});
+  it('should decode percent encoded octets', function(){
+    var link = $("<a href='http://www.example.com/%7Eusername'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com/~username";
+    assert.equal(actual, expected);
+  });
+
+  it('should decode lowercased encoded octets', function(){
+    var link = $("<a href='http://www.example.com/%7eusername'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com/~username";
+    assert.equal(actual, expected);
+  });
+
+  it('should lowercase protocol and host', function(){
+    var link = $("<a href='HTTP://www.Example.com'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com";
+    assert.equal(actual, expected);
+  });
+
+  it('should remove the default port', function(){
+    var link = $("<a href='http://www.example.com:80'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com";
+    assert.equal(actual, expected);
+  });
+
+  it('should fully resolve paths', function(){
+    var link = $("<a href='http://www.example.com/foo/../bar/baz.html'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com/bar/baz.html";
+    assert.equal(actual, expected);
+  });
+
+  it('should remove trailing slash', function(){
+    var link = $("<a href='http://www.example.com/foo/'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com/foo";
+    assert.equal(actual, expected);
+  });
+
+  it('should remove directory index', function(){
+    var expected = "http://www.example.com/foo";
+
+    var link = $("<a href='http://www.example.com/foo/index.html'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    assert.equal(actual, expected);
+
+    var link = $("<a href='http://www.example.com/foo/index.php'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    assert.equal(actual, expected);
+
+    var link = $("<a href='http://www.example.com/foo/default.asp'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    assert.equal(actual, expected);
+
+    var link = $("<a href='http://www.example.com/foo/default.aspx'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    assert.equal(actual, expected);
+  });
+
+  it('should remove extra /', function(){
+    var link = $("<a href='http://www.example.com//foo/bar///baz.html'>");
+    var actual = testCrawler._normalizeUrl(link, resp);
+    var expected = "http://www.example.com/foo/bar/baz.html";
+    assert.equal(actual, expected);
+  });
 
 })
