9cebf220cf4f10398ba6ec29132d6dc2a0435981
James Culveyhouse <jculveyhouse@gmail.com>
2014-08-20 21:00:42 -0700

    Fixing a bug with the example

--- examples/hackerNews.js
+++ examples/hackerNews.js
@@ -2,10 +2,14 @@
 // Normally, you import roboto like this:
 //var roboto = require('roboto');
 
-// Note that their robots.txt defines 'Crawl-Delay: 30', thus the delay.
-
 var roboto = require('../lib/roboto');
 var html_strip = require('htmlstrip-native').html_strip;
+
+var stripOptions = {
+  include_script : false,
+  include_style : false,
+  compact_whitespace : true
+};
 
 var crawler = new roboto.Crawler({
   startUrls: [
@@ -14,6 +18,9 @@ var crawler = new roboto.Crawler({
   allowedDomains: [
     "news.ycombinator.com",
   ],
+  // Note that there is a delay due to directive 'Crawl-Delay: 30'
+  // defined in their robots.txt
+  //obeyRobotsTxt: false
 });
 
 crawler.parseField('url', function(response) {
