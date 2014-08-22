3fd9f951b428e2a0885464e466c3a02037269c9f
James Culveyhouse <jculveyhouse@gmail.com>
2014-08-22 15:07:44 -0700

    Added an 'httpError' event

---
+++ test/events.js
@@ -0,0 +1,44 @@
+var assert = require("assert");
+var roboto = require('../lib/roboto');
+var Log = require('log');
+
+var fixtures = require('./fixtures');
+var mockserver = null;
+var crawler = null;
+
+process.env['NODE_ENV'] = 'test';
+
+describe('Happy Path', function(){
+  beforeEach(function() {
+    mockserver = require('./mockserver').createServer(9999);
+    crawler = fixtures.storiesCrawler();
+  });
+
+  afterEach(function() {
+    delete crawler;
+    mockserver.close();
+  });
+
+  it('should emit httpError events', function(done){
+    var num404s = 0;
+    crawler.on('httpError', function(statusCode, href, referer) {
+      assert.equal(href, 'http://localhost:9999/stories/page3.html');
+      assert.equal(referer.href, 'http://localhost:9999/stories/page1.html');
+      if (statusCode === 404) {
+        num404s += 1;
+      }
+    });
+
+    crawler.once('finish', function(){
+      assert.equal(num404s, 1);
+      done();
+    });
+
+    crawler.crawl();
+  })
+
+})
+
+
+
+
